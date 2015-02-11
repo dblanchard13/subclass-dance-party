7cd35db0631b36a51e5c3966e58c9a6111eb1fc7
dblanchard13 <davidb.prof@gmail.com>
2015-02-10 18:31:33 -0800

    Dance party had to copy files from another repo to band-aid a git issue.

--- src/init.js
+++ src/init.js
@@ -22,12 +22,83 @@ $(document).ready(function(){
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
+    window.dancers.push(dancer);
+    $('.song').get(0).play();
   });
+
+  $(".lineUpButton").on("click", function(event){
+    var lineUpFunctionName = $(this).data('lineUp-function-name');
+    var lineUpFunction = window[lineUpFunctionName];
+
+      var $children = $('body').find('.dancer');
+      $.each($children, function(){
+        console.log($(this));
+        $(this).finish();
+        $(this).css({'left':'40px'});
+
+
+      });
+  });
+
+  $(".pairUpButton").on("click", function(event){
+    var pairUpFunctionName = $(this).data('pairUp-function-name');
+    var pairUpFunction = window[pairUpFunctionName];
+    var windowArray = [];
+    if(window.dancers.length > 1){
+      $('body').find('.dancer').remove()
+    };
+    var pairUp = function(){
+    var closest = 0;
+    var closestVal = 20000;
+    while (0 < window.dancers.length) {
+      var value = window.dancers.pop();
+      if (window.dancers.length>0){
+        for (var y = 0, count = window.dancers.length; y < count; y++) {
+          var hypotDiff = Math.sqrt(Math.pow((window.dancers[y].top - value.top), 2) + Math.pow((window.dancers[y].left - value.left), 2));
+          if (hypotDiff < closestVal) {
+            closestVal = hypotDiff;
+            closest = y;
+          }
+        }
+          var left = value.left + 30;
+          window.dancers[closest].setPosition(value.top, left);
+          $('body').append(window.dancers[closest].$node);
+          var placeholder = window.dancers.splice(closest, 1)[0];
+          windowArray.push(placeholder);
+      }
+      $('body').append(value.$node);
+      windowArray.push(value);
+
+      closest = 0;
+      closestVal = 20000;
+    }
+
+      window.dancers = windowArray;
+    };
+    pairUp();
+  });
+
+  $('body').on('mouseenter', '.dancer', function(){
+    $('body').css({'background-image':'url("pic/rick-astley-80s-hairstyle.jpg")'});
+  });
+
+$('body').on('mouseleave', '.dancer', function(){
+    $('body').css({'background-image':'none'})
+  });
+
 });
+
+
+
+
+
+
+
+
 
---
+++ src/rickAstley.js
@@ -0,0 +1,14 @@
+var makeRickAstley = function(top, left, timeBetweenSteps){
+  makePopDancer.call(this, top, left, timeBetweenSteps);
+};
+
+makeRickAstley.prototype = Object.create(makePopDancer.prototype);
+makeRickAstley.prototype.constructor = makeRickAstley;
+
+makeRickAstley.prototype.step = function(){
+  makePopDancer.prototype.step.call(this);
+  var value = $("body").width() * Math.random() + 'px';
+  this.$node.addClass("rick");
+  this.$node.animate({left: value});
+
+};
